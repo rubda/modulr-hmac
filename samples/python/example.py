f9d20695829de16e13c609cdab9b8ec68d1bb9b0
Ruben Das <ruben.das@tink.se>
2021-05-05 11:49:26 +0200

    poc

--- samples/python/example.py
+++ samples/python/example.py
@@ -1,23 +1,62 @@
 import requests
-from modulr_hmac.api_auth  import Signature
+import json
+from modulr_hmac.api_auth import Signature
 
-API_KEY = "57502612d1bb2c00010000256c9568f1f7d540e085052b425d46d233"
-API_SECRET = "Y2Q0MzhmZDE2OWIzNDVkNGE5ZTczOTU3ZTAxZWY5NTc="
+API_KEY = ""
+API_SECRET = ""
+API_BASE= "https://api-sandbox.modulrfinance.com/api-sandbox"
 
-url = f'https://api-sandbox.modulrfinance.com/api-sandbox/customers'
+def _get_headers():
+    signature = Signature(API_KEY, API_SECRET)
+    result = signature.calculate()
+    return result.get_http_headers()
 
+def get_customers():
+    url = f"{API_BASE}/customers"
+    response = requests.get(url, headers=_get_headers())
+    return response
 
-# Set up signature helper
-signature = Signature(API_KEY, API_SECRET)
+def get_accounts(customer):
+    url = f"{API_BASE}/customers/{customer}/accounts"
+    response = requests.get(url, headers=_get_headers())
+    return response
 
-# calculate mhac signature with current date time and random nonce
-result = signature.calculate()
+def get_account(account):
+    url = f"{API_BASE}/accounts/{account}"
+    response = requests.get(url, headers=_get_headers())
+    return response
 
-# call the api with requireed headers
-response = requests.get(url, headers=result.get_http_headers())
+def create_account(customer, currency, reference, product_code):
+    url = f"{API_BASE}/customers/{customer}/accounts"
+    payload = {"currency": currency, "externalReference": reference, "productCode": product_code}
+    response = requests.post(url, json = payload, headers=_get_headers())
+    return response
 
-if response.status_code != 200:
-    print(f'Unsuccessful API call, code: {response.status_code}, body: {response.text}')
-else:
-    print(response.text)
+def create_payment():
+    url = f"{API_BASE}/credit"
+    payload = {"payerDetail":{"identifier":{"accountNumber":"70469265","sortCode":"000000","type":"SCAN"},"name":"Payee"},"accountId":"A120MC23","amount":1000,"type":"PI_BACS","description":"Polish Consulting SA"}
+    response = requests.post(url, json = payload, headers=_get_headers())
+    return response
 
+def get_account_transactions(account):
+    url = f"{API_BASE}/accounts/{account}/transactions?page=0&size=20"
+    response = requests.get(url, headers=_get_headers())
+    return response
+
+if __name__ == "__main__":
+    #customers = get_customers()
+    #print(json.dumps(customers.json(), indent=2))
+
+    #account = create_account("C120G9MM", "GBP", "Python 4 life", "O1200001")
+    #print(json.dumps(account.json(), indent=2))
+
+    #accounts = get_accounts("C120G9MM")
+    #print(json.dumps(accounts.json(), indent=2))
+
+    #payment = create_payment()
+
+    #transactions = get_account_transactions("A120MC23")
+    #print(json.dumps(transactions.json(), indent=2))
+
+    account = get_account("A120MC23")
+    print(json.dumps(account.json(), indent=2))
